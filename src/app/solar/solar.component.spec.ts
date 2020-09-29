ecafdadb624b4ea2678d94f7e6bb30e1a2c1de3e
tanusha <tanushajain1@gmail.com>
2020-09-29 10:42:53 +0530

    First commit

---
+++ src/app/solar/solar.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SolarComponent } from './solar.component';
+
+describe('SolarComponent', () => {
+  let component: SolarComponent;
+  let fixture: ComponentFixture<SolarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SolarComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SolarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
