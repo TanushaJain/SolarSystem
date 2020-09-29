470af8aaac88620f3b73778226d24c3dbcaa80a6
tanusha <tanushajain1@gmail.com>
2020-09-29 10:42:53 +0530

    First commit

---
+++ src/app/solar/solar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-solar',
+  templateUrl: './solar.component.html',
+  styleUrls: ['./solar.component.css']
+})
+export class SolarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
